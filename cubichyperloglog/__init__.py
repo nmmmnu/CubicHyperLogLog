5bd0f8c6516bfd9dd83daa6caa0b8e4445d15e0d
Nikolay Mihaylov <nmmm@xdell5520.(none)>
2013-04-22 16:04:56 +0300

    -

---
+++ cubichyperloglog/__init__.py
@@ -0,0 +1,13 @@
+
+from chll	import CubicHyperLogLog
+from chllredis	import CubicHyperLogLogRedis
+from chllcassa	import CubicHyperLogLogCassandra
+
+__author__	= "Nikolay Mihaylov (nmmm@nmmm.nu)"
+__version__	= "0.9.x"
+__copyright__	= "Copyright (c) 2013 Nikolay Mihaylov"
+__license__	= "GPL v.3"
+
+#__version_info__ = (0, 9, 0)
+
+
